6c4ae5fea7d48b40162d9fd9896a41e5f2ebe551
huijjj <hui0213@postech.ac.kr>
2022-06-28 11:34:17 +0000

    WIP

--- toy_conv.c
+++ toy_conv.c
@@ -5,12 +5,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <memory.h>
-#include <math.h>
-#include <arm_neon.h>
-
 
 #define NUMTHREAD 4
-#define UNUSED NULL
+#define UNUSED 0
 
 int _N;
 int _IH;
@@ -24,6 +21,8 @@ int32_t* _tensorIn;
 int32_t* _tensorOut;
 int32_t* _kernel;
 
+int inference(int32_t*, int32_t*, int32_t*, int, int, int, int, int, int, int);
+
 double benchmark(
     int32_t *tensorIn,
     int32_t *kernel,
@@ -33,7 +32,7 @@ double benchmark(
     int KH, int KW
 )
 {
-    int num_iter = 1; // originally 500
+    int num_iter = 500; // originally 500
 
     struct timespec start, end;
     double total_time = 0;
@@ -75,30 +74,9 @@ inline uint8_t c_in(int n, int r, int c, int32_t* st) {
 typedef struct {
     int t;
     void** out;
-    void* in;
     void** ker;
 } args;
 
-
-void* img2col(void* arg) {
-    int t = ((args*)arg)->t;
-    uint8_t* _out = (uint8_t*)malloc(sizeof(uint8_t) * _IH * _IW * _IC * _KH * _KW * _N / NUMTHREAD);
-    
-    *(((args*)arg)->out) = _out;
-
-    for(int n = 0; n < (_N / NUMTHREAD); n++) {
-        for(int r = 0; r < _IH*_IW; r++) {
-            for(int c = 0; c < _IC*_KH*_KW; c++) {
-                // _out[r * _IC*_KH*_KW + c] = c_in(n, c, r + t * (_IH * _IW / NUMTHREAD), _tensorIn);
-                // _out[n * _IH*_IW*_IC*_KH*_KW + r * _IC*_KH*_KW + c] = c_in(n + t * (_N/NUMTHREAD), c, r, _tensorIn);
-                *(_out) = c_in(n + t * (_N/NUMTHREAD), c, r, _tensorIn);
-                _out = _out + 1;
-            }
-        }
-    }
-
-    pthread_exit(NULL);
-}
 
 void* ker2col(void* arg) {
     int t = ((args*)arg)->t;
@@ -108,7 +86,6 @@ void* ker2col(void* arg) {
 
     for(int r = 0; r < (_OC / NUMTHREAD); r++) {
         for(int c = 0; c < _IC*_KH*_KW; c++) {
-            // _out[r*_IC*_KH*_KW + c] = c_ker(r + t * (_OC/NUMTHREAD), c, _kernel);
             *(_out) = c_ker(r + t * (_OC/NUMTHREAD), c, _kernel);
             _out = _out + 1;
         }
@@ -117,27 +94,37 @@ void* ker2col(void* arg) {
     pthread_exit(NULL);
 }
 
-void* matmul_naive(void* arg) {
+void* img2col_matmul(void* arg) {
     int t = ((args*)arg)->t;
-    uint8_t* in = ((args*)arg)->in;
     uint8_t** ker = ((args*)arg)->ker;
 
-    int32_t* out = _tensorOut + t * (_N/NUMTHREAD)*_IH*_IW*_OC;
+    int32_t* _out = _tensorOut + t * (_N / NUMTHREAD) * _IH * _IW * _OC;
+    uint8_t _img[_IH * _IW * _IC * _KH * _KW * _N / NUMTHREAD];
+    uint8_t* __img = _img;
 
+    for(int n = 0; n < (_N / NUMTHREAD); n++) {
+        for(int r = 0; r < _IH*_IW; r++) {
+            for(int c = 0; c < _IC*_KH*_KW; c++) {
+                *(__img) = c_in(n + t * (_N/NUMTHREAD), c, r, _tensorIn);
+                __img = __img + 1;
+            }
+        }
+    }
+
+    register int32_t temp;
     for(int n = 0; n < _N / NUMTHREAD; n++) {
         for(int i = 0; i < _IH*_IW; i++) {
             for(int j = 0; j < _OC; j++) {
-                int32_t temp = 0;
+                temp = 0;
                 for(int k = 0; k < _IC*_KH*_KW; k++) {
-                    temp += in[n * _IH*_IW*_IC*_KH*_KW + i * _IC*_KH*_KW + k] * ker[j / (_OC / NUMTHREAD)][(j % (_OC / NUMTHREAD)) * _IC*_KH*_KW + k];
+                    temp = temp + _img[n * _IH*_IW*_IC*_KH*_KW + i * _IC*_KH*_KW + k] * ker[j / (_OC / NUMTHREAD)][(j % (_OC / NUMTHREAD)) * _IC*_KH*_KW + k];
                 }
-                // _out[n * _IH*_IW*_OC + i * _OC + j] = temp;
                 *(_out) = temp;
                 _out = _out + 1;
             }
         }
     }
-
+    
     pthread_exit(NULL);
 }
 
@@ -177,10 +164,10 @@ int inference(
     uint8_t* b_ker = NULL;
     uint8_t* c_ker = NULL;
     uint8_t* d_ker = NULL;
-    args _a_arg = { 0, &a_ker, UNUSED, UNUSED };
-    args _b_arg = { 1, &b_ker, UNUSED, UNUSED };
-    args _c_arg = { 2, &c_ker, UNUSED, UNUSED };
-    args _d_arg = { 3, &d_ker, UNUSED, UNUSED };
+    args _a_arg = { 0, &a_ker, UNUSED };
+    args _b_arg = { 1, &b_ker, UNUSED };
+    args _c_arg = { 2, &c_ker, UNUSED };
+    args _d_arg = { 3, &d_ker, UNUSED };
     pthread_create(&a, NULL, ker2col, &_a_arg);
     pthread_create(&b, NULL, ker2col, &_b_arg);
     pthread_create(&c, NULL, ker2col, &_c_arg);
@@ -190,49 +177,19 @@ int inference(
     pthread_join(c, &c_st);
     pthread_join(d, &d_st);
 
-
-    uint8_t* a_in = NULL;
-    uint8_t* b_in = NULL;
-    uint8_t* c_in = NULL;
-    uint8_t* d_in = NULL;
-    args a_arg = { 0, &a_in, UNUSED, UNUSED };
-    args b_arg = { 1, &b_in, UNUSED, UNUSED };
-    args c_arg = { 2, &c_in, UNUSED, UNUSED };
-    args d_arg = { 3, &d_in, UNUSED, UNUSED };
-    pthread_create(&a, NULL, img2col, &a_arg);
-    pthread_create(&b, NULL, img2col, &b_arg);
-    pthread_create(&c, NULL, img2col, &c_arg);
-    pthread_create(&d, NULL, img2col, &d_arg);
-    pthread_join(a, &a_st);
-    pthread_join(b, &b_st);
-    pthread_join(c, &c_st);
-    pthread_join(d, &d_st);
-
     uint8_t* _ker[] = { a_ker, b_ker, c_ker, d_ker };
-    int32_t* b_out = NULL;
-    int32_t* c_out = NULL;
-    int32_t* d_out = NULL;
-    args a_arg_ = { 0, UNUSED, a_in, _ker };
-    args b_arg_ = { 1, &b_out, b_in, _ker };
-    args c_arg_ = { 2, &c_out, c_in, _ker };
-    args d_arg_ = { 3, &d_out, d_in, _ker };
-    pthread_create(&a, NULL, matmul_naive, &a_arg_);
-    pthread_create(&b, NULL, matmul_naive, &b_arg_);
-    pthread_create(&c, NULL, matmul_naive, &c_arg_);
-    pthread_create(&d, NULL, matmul_naive, &d_arg_);
+    args a_arg_ = { 0, UNUSED, _ker };
+    args b_arg_ = { 1, UNUSED, _ker };
+    args c_arg_ = { 2, UNUSED, _ker };
+    args d_arg_ = { 3, UNUSED, _ker };
+    pthread_create(&a, NULL, img2col_matmul, &a_arg_);
+    pthread_create(&b, NULL, img2col_matmul, &b_arg_);
+    pthread_create(&c, NULL, img2col_matmul, &c_arg_);
+    pthread_create(&d, NULL, img2col_matmul, &d_arg_);
     pthread_join(a, &a_st);
     pthread_join(b, &b_st);
     pthread_join(c, &c_st);
     pthread_join(d, &d_st);
-
-    free(a_in);
-    free(b_in);
-    free(c_in);
-    free(d_in);
-
-    free(b_out);
-    free(c_out);
-    free(d_out);    
 
     free(a_ker);
     free(b_ker);
